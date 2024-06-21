5b207dfdbb5a888c20799b29c789f445491e1e7f
Raikitin <147205380+Raikitin@users.noreply.github.com>
2024-06-21 11:02:06 +0200

    added main

---
+++ P3/main.py
@@ -0,0 +1,11 @@
+import gym
+import fh_ac_ai_gym
+
+wumpus_env = gym.make('Wumpus-v0')
+wumpus_env.reset()
+wumpus_env.render()
+
+while True:
+    action = int(input("Action: "))
+    wumpus_env.step(action)
+    wumpus_env.render()
