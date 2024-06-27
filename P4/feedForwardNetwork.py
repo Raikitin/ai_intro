4a2bc81b4f24ba994625e4d886ccc4db71b8231e
Raikitin <147205380+Raikitin@users.noreply.github.com>
2024-06-27 13:23:37 +0200

    4.2 close to finish ig, problem with test data import

--- P4/feedForwardNetwork.py
+++ P4/feedForwardNetwork.py
@@ -1,8 +1,9 @@
 import math
 import random
 import numpy as np
-import matplotlib.pyplot as plt
-from keras.datasets import mnist    # TODO loo up best way to load training data
+#import matplotlib.pyplot as plt
+from keras.datasets import mnist  # TODO loo up best way to load training data
+
 
 class FeedForwardNetwork:
     def __init__(self, input, hidden, output, rate, data):
@@ -12,15 +13,15 @@ class FeedForwardNetwork:
         self.rate = rate
         self.data = data
 
-        self.Wih = np.random.randn(self.input, self.hidden)     # input-hidden weight
-        self.Who = np.random.randn(self.hidden, self.output)    # hidden-output weight
+        self.Wih = np.random.randn(self.input, self.hidden)  # input-hidden weight
+        self.Who = np.random.randn(self.hidden, self.output)  # hidden-output weight
 
         (train_x, train_y), (self.test_x, self.test_y) = mnist.load_data()
         self.train_x = []
         self.train_y = []
 
         for i in range(self.data):
-            self.train_x.append(self) # TODO image conversion in self
+            self.train_x.append(self.image_convert(np.array(train_x[i], dtype=float).flatten()))
 
             tmp = np.array([0.01 for i in range(self.output)], dtype=float)
             tmp[train_y[i]] = 0.99
@@ -43,14 +44,57 @@ class FeedForwardNetwork:
 
         return output, hidden
 
+    def train(self, iteration):
+        for i in range(iteration):
+            out, hidden = self.think(self.train_x)
 
-    #def train(self, iteration):
-    #    for i in range(iteration):
+            error_output = self.train_y - out
 
+            error_hidden = np.dot(np.transpose(self.Who), error_output)
 
+            #tmp = np.vectorize(self.sigmoid)(out)
+
+            delta_who = np.dot((error_output * np.vectorize(self.sigmoid)(out)), np.transpose(hidden))
+
+            delta_wih = np.dot((error_hidden * np.vectorize(self.sigmoid)(hidden)), np.transpose(self.train_x))
+
+            self.Who = self.rate * delta_who
+            self.Wih = self.rate * delta_wih
 
     #def eval(self)
 
-    #def scorecard(self)
+    def image_convert(self, image):
+        input = np.zeros(len(image), dtype=float)
+        factor = 0.99 / 254
 
-#if __name__ == '__main__':
+        for i in range(len(image)):
+            if (image[i] == np.unit8(0)):
+                input[i] = 0.01
+            else:
+                input[i] = image[i] * factor
+        return input
+
+    def scorecard(self, it):
+        fails = 0
+        for i in range(it):
+            input = self.image_convert(np.array(self.test_x[i]).flatten())
+            o, h = self.think([input])
+            if (np.argmax(o) != self.test_y[i]):
+                fails += 1
+        print("Iteration count: " + str(it) + ", Accuracy: " + str((it - fails) / it) + "%")
+
+
+if __name__ == '__main__':
+    number_input_nodes = 784
+    number_hidden_nodes = 200
+    number_output_nodes = 10
+    training_samples = 300
+    training_amount = 300
+    test_amount = 100
+    learning_rate = 0.1
+
+    network = FeedForwardNetwork(number_input_nodes, number_hidden_nodes, number_output_nodes, learning_rate,
+                                 training_samples)
+    network.train(training_amount)
+    # score
+    network.scorecard(test_amount)
